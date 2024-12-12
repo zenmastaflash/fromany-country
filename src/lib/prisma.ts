15b6936753b0608e33c436e1d3e642549e2a8848
KO <kotterst@gmail.com>
2024-12-12 17:09:02 +0100

    fix: handle special characters in database URL

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -6,6 +6,11 @@ export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
     log: ['query', 'info', 'warn', 'error'],
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL?.replace(/\\@/g, '@')
+      }
+    }
   });
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;

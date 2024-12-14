154f0ce768f6bdea582f8160de3ed14b3a3b53e3
KO <kotterst@gmail.com>
2024-12-14 17:04:34 +0100

    fix: simplify auth config

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,16 @@
 import NextAuth from 'next-auth'
-import { authOptions } from './auth'
+import GoogleProvider from 'next-auth/providers/google'
+import { PrismaAdapter } from '@auth/prisma-adapter'
+import prisma from '@/lib/prisma'
 
-const handler = NextAuth(authOptions)
+const handler = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [GoogleProvider({
+    clientId: process.env.GOOGLE_CLIENT_ID!,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+  })],
+  secret: process.env.NEXTAUTH_SECRET
+})
 
-export { handler as GET, handler as POST }
+export const GET = handler
+export const POST = handler

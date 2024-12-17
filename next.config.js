7b452001d231e74dd24a7b6ddce3c72d0a40d0a1
KO <kotterst@gmail.com>
2024-12-17 20:50:40 +0100

    fix: remove Prisma schema and simplify Next.js config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    unoptimized: true
-  }
+  poweredByHeader: false,
 }
 
 module.exports = nextConfig

e233984a4a19509b7e8a980eb8153ef939af43ef
KO <kotterst@gmail.com>
2024-12-18 14:14:58 +0100

    Simplify route handler to import from dedicated auth file

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,19 +1,3 @@
-import NextAuth from 'next-auth';
-import GoogleProvider from 'next-auth/providers/google';
-
-export const { handlers, auth, signIn, signOut } = NextAuth({
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID ?? '',
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? ''
-    })
-  ],
-  callbacks: {
-    async session({ session, token }) {
-      session.user.id = token.sub;
-      return session;
-    }
-  }
-});
+import { handlers } from '@/auth';
 
 export const { GET, POST } = handlers;

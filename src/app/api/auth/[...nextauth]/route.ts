f0c7bc456fcaf788f3edc63140247f79574466d6
KO <kotterst@gmail.com>
2024-12-18 14:08:28 +0100

    Explicitly configure NextAuth route handler for Next.js 14

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,17 +1,24 @@
-import type { NextAuthConfig } from 'next-auth';
+import { type NextRequest } from 'next/server';
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 
-const config = {
+export const runtime = 'nodejs';
+
+const authOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID ?? '',
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? ''
     })
-  ],
-  session: { strategy: 'jwt' },
-} satisfies NextAuthConfig;
+  ]
+};
 
-const { handlers } = NextAuth(config);
+const handler = NextAuth(authOptions);
 
-export const { GET, POST } = handlers;
+export function GET(req: NextRequest) {
+  return handler(req);
+}
+
+export function POST(req: NextRequest) {
+  return handler(req);
+}

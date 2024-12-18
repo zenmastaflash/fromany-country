707e0fb34f59a13a46530ab4904cf1379cbc7f55
KO <kotterst@gmail.com>
2024-12-18 14:14:50 +0100

    Separate auth configuration into dedicated file

--- src/auth.ts
+++ src/auth.ts
@@ -1,29 +1,18 @@
 import NextAuth from 'next-auth';
-import type { NextAuthConfig } from 'next-auth';
-import Google from 'next-auth/providers/google';
+import GoogleProvider from 'next-auth/providers/google';
 
-const config = {
+export const { handlers, auth, signIn, signOut } = NextAuth({
   providers: [
-    Google({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID ?? '',
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? ''
+    })
   ],
+  session: { strategy: 'jwt' },
   callbacks: {
-    authorized({ auth, request: { nextUrl } }) {
-      const isLoggedIn = !!auth?.user;
-      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
-      if (isOnDashboard) {
-        if (isLoggedIn) return true;
-        return false; // Redirect unauthenticated users to login page
-      } else if (isLoggedIn) {
-        return Response.redirect(new URL('/dashboard', nextUrl));
-      }
-      return true;
-    },
-  },
-} satisfies NextAuthConfig;
-
-export const { auth, signIn, signOut, handlers } = NextAuth(config);
-
-export type AuthClient = typeof auth;
+    async session({ session, token }) {
+      session.user.id = token.sub;
+      return session;
+    }
+  }
+});

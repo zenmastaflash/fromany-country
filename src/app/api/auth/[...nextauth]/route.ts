62e4c681df0fccfa7fa4341c52fb0c8133156972
KO <kotterst@gmail.com>
2024-12-24 14:20:03 +0100

    Update route.ts

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,1 +1,8 @@
-export { GET, POST } from '@/lib/auth'
+import NextAuth from "next-auth";
+import { authConfig } from "@/lib/auth";
+
+// Create a NextAuth handler from your config
+const handler = NextAuth(authConfig);
+
+// Export as GET and POST
+export { handler as GET, handler as POST };

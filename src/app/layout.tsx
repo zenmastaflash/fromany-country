e87decf70ca307199fe2234b32f5797c3927a481
KO <kotterst@gmail.com>
2024-12-24 15:08:05 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,9 @@ export const metadata = {
   description: 'Document management for digital nomads',
 };
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 export default function RootLayout({
   children,
 }: {

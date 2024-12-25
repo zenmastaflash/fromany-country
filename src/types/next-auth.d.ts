c67bf381d2421daee2cca555ab488c7b4cc94d38
KO <kotterst@gmail.com>
2024-12-25 11:28:45 +0100

    Update next-auth.d.ts

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -3,11 +3,11 @@ import { DefaultSession } from 'next-auth'
 declare module 'next-auth' {
   interface Session {
     user: {
-      id: number
+      id: string
     } & DefaultSession['user']
   }
   
   interface User {
-    id: number
+    id: string
   }
 }

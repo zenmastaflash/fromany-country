1ed620073d1ad8eb0fc8cfc97bfc24c9d05985c4
KO <kotterst@gmail.com>
2024-12-25 10:59:03 +0100

    Create next-auth.d.ts

---
+++ app/types/next-auth.d.ts
@@ -0,0 +1,13 @@
+import { DefaultSession } from 'next-auth'
+
+declare module 'next-auth' {
+  interface Session {
+    user: {
+      id: number
+    } & DefaultSession['user']
+  }
+  
+  interface User {
+    id: number
+  }
+}

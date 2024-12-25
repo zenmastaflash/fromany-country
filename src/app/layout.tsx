a6c9cbcb36c4fdce0b3ea71f18501c83b656d292
KO <kotterst@gmail.com>
2024-12-25 12:16:40 +0100

    reverting header. oopsie

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import './globals.css';
 import { Inter } from 'next/font/google';
-import Header from '@/components/Header'; // Import the Header component
 
 const inter = Inter({ subsets: ['latin'] });
 

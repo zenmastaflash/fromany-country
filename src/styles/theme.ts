622523d3d12a936c4a01127d88cbf1fe68914893
KO <kotterst@gmail.com>
2024-12-26 19:46:33 +0100

    Create theme.ts

---
+++ src/styles/theme.ts
@@ -0,0 +1,20 @@
+export const theme = {
+  colors: {
+    background: '#003135',
+    dark: '#024950',
+    accent: '#964734',
+    primary: '#0FA4AF',
+    light: '#AFDDE5',
+    text: '#F0F0F0',
+  }
+} as const;
+
+// RGB values for use with opacity
+export const rgbValues = {
+  background: '0, 49, 53',
+  dark: '2, 73, 80',
+  accent: '150, 71, 52',
+  primary: '15, 164, 175',
+  light: '175, 221, 229',
+  text: '240, 240, 240',
+} as const;

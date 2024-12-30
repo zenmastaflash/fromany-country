8c25e33cf3233989a2749a5b6891f9c8ba77add3
KO <kotterst@gmail.com>
2024-12-30 15:41:54 +0100

    Update head.tsx

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,7 +1,7 @@
 export default function Head() {
   return (
     <>
-      <title>fromany.country</title>
+      <title>from any country</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Live Anywhere. Belong Everywhere." />
       <meta name="google-site-verification" content="verify" />

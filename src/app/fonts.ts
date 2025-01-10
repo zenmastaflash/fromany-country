f5c49435e527bf89b563f2c3a9d2aa87a1ac060b
KO <kotterst@gmail.com>
2025-01-10 15:03:53 +0100

    Update fonts.ts

--- src/app/fonts.ts
+++ src/app/fonts.ts
@@ -2,7 +2,9 @@ import { Inter } from 'next/font/google'
 import localFont from 'next/font/local'
 
 export const recoleta = localFont({
-  src: '../../public/fonts/recoleta-regulardemo.otf'
+  src: '../../public/fonts/recoleta-regulardemo.otf',
+  variable: '--font-recoleta', 
+  display: 'swap'
 })
 
 export const inter = Inter({

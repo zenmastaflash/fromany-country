b82eadc68c85925f6d5843f1586e59b496b5c45a
KO <kotterst@gmail.com>
2025-01-10 14:47:14 +0100

    Update fonts.ts

--- src/app/fonts.ts
+++ src/app/fonts.ts
@@ -2,8 +2,8 @@ import { Inter } from 'next/font/google'
 import localFont from 'next/font/local'
 
 export const salisbury = localFont({
-  src: '../../public/fonts/SalisburyBold.ttf',
-  variable: '--font-salisbury',
+  src: '../../public/fonts/recoleta-regulardemo.otf',
+  variable: '--font-recoleta',
   display: 'swap',
 })
 

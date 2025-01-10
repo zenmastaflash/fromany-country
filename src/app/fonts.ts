f48cb20baa10b8d00a95c198e3e332e00d0bf9d3
KO <kotterst@gmail.com>
2025-01-10 15:18:16 +0100

    Update fonts.ts

--- src/app/fonts.ts
+++ src/app/fonts.ts
@@ -1,14 +1,22 @@
 import { Inter } from 'next/font/google'
 import localFont from 'next/font/local'
 
-export const recoleta = localFont({
-  src: '../../public/fonts/Recoleta Regular.otf',
-  variable: '--font-recoleta', 
-  display: 'swap'
-})
-
+// Inter configuration with proper weight ranges
 export const inter = Inter({
   subsets: ['latin'],
   display: 'swap',
   variable: '--font-inter',
+  // Adding common weights
+  weight: ['400', '500', '600', '700'],
+})
+
+// Updated Recoleta configuration
+export const recoleta = localFont({
+  src: [{
+    path: '../../public/fonts/Recoleta Regular.otf',
+    weight: '400',
+    style: 'normal'
+  }],
+  variable: '--font-recoleta',
+  display: 'swap',
 })

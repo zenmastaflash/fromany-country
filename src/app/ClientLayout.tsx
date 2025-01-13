b5d5ddb5e899f04b15b3afb3417900b1dc997af6
KO <kotterst@gmail.com>
2025-01-13 10:58:58 +0100

    Update ClientLayout.tsx

--- src/app/ClientLayout.tsx
+++ src/app/ClientLayout.tsx
@@ -6,7 +6,7 @@ import { SessionProvider } from "next-auth/react";
 export default function ClientLayout({ children }: { children: React.ReactNode }) {
   return (
     <SessionProvider>
-<div className="min-h-screen bg-gradient-to-br from-secondary/70 via-primary/50 to-accent/60">
+<div className="min-h-screen bg-gradient-to-br from-secondary/70 via-primary/40 to-accent/60">
   <Header />
         <main>
           {children}

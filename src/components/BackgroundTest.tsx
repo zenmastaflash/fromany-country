e6d91b6fb57fadd982e0efcd5e58cdec11c0591f
KO <kotterst@gmail.com>
2025-01-13 11:57:44 +0100

    Update BackgroundTest.tsx

--- src/components/BackgroundTest.tsx
+++ src/components/BackgroundTest.tsx
@@ -9,11 +9,13 @@ const BackgroundTest: React.FC = () => {
       <Card className="relative overflow-hidden h-[80vh] bg-secondary">
         {/* Ripple circles */}
         <div className="absolute w-full h-full">
-          {/* Largest to smallest, secondary -> accent */}
-          <div className="circle circle-5 bg-secondary" />
-          <div className="circle circle-4 bg-secondary/90" />
-          <div className="circle circle-3 bg-accent/60" />
-          <div className="circle circle-2 bg-accent/80" />
+          {/* Largest to smallest: secondary -> primary -> accent */}
+          <div className="circle circle-7 bg-secondary" />
+          <div className="circle circle-6 bg-secondary/90" />
+          <div className="circle circle-5 bg-primary/90" />
+          <div className="circle circle-4 bg-primary" />
+          <div className="circle circle-3 bg-primary/90" />
+          <div className="circle circle-2 bg-accent/90" />
           <div className="circle circle-1 bg-accent" /> {/* Accent at full opacity */}
         </div>
         
@@ -27,46 +29,62 @@ const BackgroundTest: React.FC = () => {
             position: absolute;
             border-radius: 50%;
             animation: ripple 15s infinite;
-            transform-origin: bottom right;
+            transform-origin: center center;
           }
 
           .circle-1 {
             width: 100vw;
             height: 100vw;
-            right: 0;
-            bottom: 0;
+            right: -50vw;  /* Center at corner */
+            bottom: -50vw;
             animation-delay: 0s;
           }
 
           .circle-2 {
             width: 120vw;
             height: 120vw;
-            right: 0;
-            bottom: 0;
-            animation-delay: 0.3s;
+            right: -60vw;
+            bottom: -60vw;
+            animation-delay: 0.2s;
           }
 
           .circle-3 {
             width: 140vw;
             height: 140vw;
-            right: 0;
-            bottom: 0;
-            animation-delay: 0.6s;
+            right: -70vw;
+            bottom: -70vw;
+            animation-delay: 0.4s;
           }
 
           .circle-4 {
             width: 160vw;
             height: 160vw;
-            right: 0;
-            bottom: 0;
-            animation-delay: 0.9s;
+            right: -80vw;
+            bottom: -80vw;
+            animation-delay: 0.6s;
           }
 
           .circle-5 {
             width: 180vw;
             height: 180vw;
-            right: 0;
-            bottom: 0;
+            right: -90vw;
+            bottom: -90vw;
+            animation-delay: 0.8s;
+          }
+
+          .circle-6 {
+            width: 200vw;
+            height: 200vw;
+            right: -100vw;
+            bottom: -100vw;
+            animation-delay: 1.0s;
+          }
+
+          .circle-7 {
+            width: 220vw;
+            height: 220vw;
+            right: -110vw;
+            bottom: -110vw;
             animation-delay: 1.2s;
           }
 

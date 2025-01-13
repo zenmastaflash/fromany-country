96d11464acab12dfa7dea44ed4b0e5f09ad06e44
KO <kotterst@gmail.com>
2025-01-13 11:51:30 +0100

    Update BackgroundTest.tsx

--- src/components/BackgroundTest.tsx
+++ src/components/BackgroundTest.tsx
@@ -6,14 +6,14 @@ const BackgroundTest: React.FC = () => {
     <div className="p-8 space-y-8">
       <h2 className="text-2xl font-bold text-text">Background Animation Tests</h2>
       
-      <Card className="relative overflow-hidden h-[80vh] bg-transparent">
+      <Card className="relative overflow-hidden h-[80vh] bg-secondary">
         {/* Ripple circles */}
         <div className="absolute w-full h-full">
-          {/* Largest to smallest, secondary -> primary -> accent */}
-          <div className="circle circle-5 bg-secondary/90" />
-          <div className="circle circle-4 bg-secondary/80" />
-          <div className="circle circle-3 bg-primary/70" />
-          <div className="circle circle-2 bg-primary/60" />
+          {/* Largest to smallest, secondary -> accent */}
+          <div className="circle circle-5 bg-secondary" />
+          <div className="circle circle-4 bg-secondary/90" />
+          <div className="circle circle-3 bg-accent/60" />
+          <div className="circle circle-2 bg-accent/80" />
           <div className="circle circle-1 bg-accent" /> {/* Accent at full opacity */}
         </div>
         
@@ -27,59 +27,63 @@ const BackgroundTest: React.FC = () => {
             position: absolute;
             border-radius: 50%;
             animation: ripple 15s infinite;
+            transform-origin: bottom right;
           }
 
           .circle-1 {
             width: 100vw;
             height: 100vw;
-            right: -20vw;
-            bottom: -20vw;
+            right: 0;
+            bottom: 0;
             animation-delay: 0s;
           }
 
           .circle-2 {
             width: 120vw;
             height: 120vw;
-            right: -25vw;
-            bottom: -25vw;
+            right: 0;
+            bottom: 0;
             animation-delay: 0.3s;
           }
 
           .circle-3 {
             width: 140vw;
             height: 140vw;
-            right: -30vw;
-            bottom: -30vw;
+            right: 0;
+            bottom: 0;
             animation-delay: 0.6s;
           }
 
           .circle-4 {
             width: 160vw;
             height: 160vw;
-            right: -35vw;
-            bottom: -35vw;
+            right: 0;
+            bottom: 0;
             animation-delay: 0.9s;
           }
 
           .circle-5 {
             width: 180vw;
             height: 180vw;
-            right: -40vw;
-            bottom: -40vw;
+            right: 0;
+            bottom: 0;
             animation-delay: 1.2s;
           }
 
           @keyframes ripple {
             0% {
               transform: scale(0.8);
+              opacity: 1;
             }
             
             50% {
               transform: scale(1.3);
+              opacity: 0.5;
             }
             
             100% {
               transform: scale(0.8);
+              opacity: 0;
             }
           }
         `}</style>

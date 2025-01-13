26d4b47fa5159f5c1d7074c19cb48f52350456ea
KO <kotterst@gmail.com>
2025-01-13 10:12:53 +0100

    Update Header.tsx to match font

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -45,7 +45,7 @@ export default function Header() {
       <nav className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8" aria-label="Top">
         <div className="flex h-16 items-center justify-between">
           <div className="flex items-center">
-            <Link href="/" className="text-2xl font-bold text-text">
+            <Link href="/" className="text-2xl font-bold text-text heading-font">
               from any country
             </Link>
           </div>

9b67391ff8c391a637fa65babdeee17aaa6a6112
KO <kotterst@gmail.com>
2025-01-25 22:37:27 +0100

    Update and rename rpute.ts to route.ts

--- src/app/api/travel/[id]/route.ts
+++ src/app/api/travel/[id]/route.ts
@@ -4,6 +4,22 @@ import { getServerSession } from 'next-auth/next';
 import { prisma } from '@/lib/prisma';
 import { NextResponse } from 'next/server';
 
+export async function PATCH(
+  request: Request,
+  { params }: { params: { id: string } }
+) {
+  const session = await getServerSession(authConfig);
+  if (!session?.user?.email) return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+
+  const data = await request.json();
+  const travel = await prisma.travel.update({
+    where: { id: params.id },
+    data
+  });
+
+  return NextResponse.json(travel);
+}
+
 export async function DELETE(
   request: Request,
   { params }: { params: { id: string } }
@@ -17,7 +33,6 @@ export async function DELETE(
     await prisma.travel.delete({
       where: { id: params.id }
     });
-
     return NextResponse.json({ success: true });
   } catch (error) {
     return NextResponse.json({ error: String(error) }, { status: 500 });

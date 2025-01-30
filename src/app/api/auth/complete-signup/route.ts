f717fe8166763d7da41aea85ee0ebdad40ad7ff8
KO <kotterst@gmail.com>
2025-01-30 20:21:11 +0100

    Create sign up route.ts

---
+++ src/app/api/auth/complete-signup/route.ts
@@ -0,0 +1,30 @@
+// src/app/api/auth/complete-signup/route.ts
+import { prisma } from '@/lib/prisma';
+import { NextResponse } from 'next/server';
+import { getServerSession } from 'next-auth/next';
+import { authConfig } from '@/lib/auth';
+
+export async function POST(req: Request) {
+  try {
+    const session = await getServerSession(authConfig);
+    if (!session?.user?.email) {
+      return new NextResponse(null, { status: 401 });
+    }
+
+    const { displayName, country } = await req.json();
+
+    const user = await prisma.user.update({
+      where: { email: session.user.email },
+      data: { 
+        displayName,
+        country,
+        updated_at: new Date()
+      },
+    });
+
+    return NextResponse.json(user);
+  } catch (error) {
+    console.error(error);
+    return new NextResponse(null, { status: 500 });
+  }
+}

489b38b19b60e7c7375bba007e7e31e6a90495ee
KO <kotterst@gmail.com>
2025-02-09 16:30:00 +0100

    Update prisma.ts

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,10 +1,15 @@
 import { PrismaClient } from '@prisma/client'
+import { PrismaPg } from '@prisma/adapter-pg'
+import { Client } from '@prisma/pg-proxy'
+
+const client = new Client(process.env.DATABASE_URL!)
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
 export const prisma = globalForPrisma.prisma ?? new PrismaClient({
+  adapter: new PrismaPg(client),
   log: ['error', 'warn'],
 })
 
